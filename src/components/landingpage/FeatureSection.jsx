65c851f3bb37744c1754c29464c0d8bc2e485071
Hawsyi <hawsyidigital@gmail.com>
2025-10-09 23:38:09 +0700

    ignored

--- src/components/landingpage/FeatureSection.jsx
+++ src/components/landingpage/FeatureSection.jsx
@@ -28,16 +28,16 @@ const FeatureSection = () => {
 		},
 	];
 	return (
-		<div className="container mx-auto px-4 sm:px-6 lg:px-8">
+		<div className="container mx-auto px-6">
 			<div className="text-center">
-				<h3 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-900 mb-3 sm:mb-4">
+				<h3 className="text-3xl font-bold text-gray-900">
 					Modul Utama Aplikasi
 				</h3>
-				<p className="text-base sm:text-lg font-semibold text-secondary">
+				<p className="mt-2 font-semibold text-secondary">
 					Data terintegrasi dari empat bidang utama.
 				</p>
 			</div>
-			<div className="mt-8 sm:mt-10 lg:mt-12 grid grid-cols-1 gap-6 sm:gap-8 md:grid-cols-2 lg:grid-cols-4">
+			<div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
 				{features.map((feature, index) => (
 					<FeatureCard key={index} {...feature} />
 				))}
